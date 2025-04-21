d0f3a4a5b906cae697e606827c7505ac5c128ece
pumpkinpeasant <m2314670@edu.misis.ru>
2025-04-21 23:28:50 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,13 @@ const newMessage = ref('')
 
 function sendMessage() {
   if (newMessage.value.trim()) {
-    messages.value.push({message: newMessage.value, isDeleted: false})
+    messages.value.push({message: newMessage.value, isDeleted: false, createdAt: new Date().toLocaleString('ru-RU', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+      })})
     clearInput();
   }
 }
@@ -36,7 +42,7 @@ async function startLoop() {
 
   isLoopRunning.value = true;
 
-  while (messages.value.length > 0) {
+  while (messages.value.some(msg => !msg.isDeleted)) {
     for (const message of messages.value) {
       if (!message.isDeleted) {
         alertMessage.value = message;
@@ -57,19 +63,28 @@ watch(messages, () => {
 </script>
 
 <template>
-  <main class="d-flex justify-content-center align-items-center min-vh-100 bg-light">
-    <div class="card shadow p-4" style="width: 500px;">
-      <div class="alert alert-success d-flex gap-2 align-items-center mb-4">
+  <main class="d-flex justify-content-center align-items-center min-vh-100 bg-light p-3">
+    <div class="card rounded shadow p-4" style="width: 700px;">
+      <div class="alert alert-success d-flex gap-2 align-items-center" style="min-height: 70px">
         <button v-if="alertMessage" type="button" class="btn-close btn-sm" @click="deleteMessage(alertMessage)"/>
         <span>{{ alertMessage.message }}</span>
       </div>
 
-      <div class="border rounded p-3 mb-4" style="height: 200px; overflow-y: auto; background-color: #f8f9fa;">
+      <div class="border rounded p-3 mb-4" style="height: 400px; overflow-y: auto; background-color: #f8f9fa;">
         <div v-for="(msg, index) in messages" :key="index"
-             class="d-flex justify-content-between align-items-center mb-1">
-          <span>
-            <button v-if="msg.isDeleted" type="button" class="btn-close btn-sm"/>
-            {{ msg.message }}
+             class="d-flex gap-2 align-items-center mb-1">
+          <button
+              :style="{ visibility: msg.isDeleted ? 'visible' : 'hidden' }"
+              type="button"
+              class="btn-close btn-sm"
+          />
+          <span class="d-flex flex-column">
+            <span>
+              {{ msg.message }}
+            </span>
+            <span class="text-muted small">
+              {{ msg.createdAt }}
+            </span>
           </span>
         </div>
       </div>
@@ -77,18 +92,8 @@ watch(messages, () => {
       <div class="d-flex gap-2">
         <input @keydown.enter="sendMessage" type="text" class="form-control" v-model="newMessage"
                placeholder="Новое сообщение"/>
-        <button class="btn btn-success px-4" @click="sendMessage">ОТПРАВИТЬ</button>
+        <button class="btn btn-success px-4" @click="sendMessage">Отправить</button>
       </div>
     </div>
   </main>
 </template>
-
-<style scoped>
-.card {
-  border-radius: 12px;
-}
-
-input::placeholder {
-  color: #adb5bd;
-}
-</style>
